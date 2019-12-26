14558f2d96f085291e6045c9adda4ab7519e2962
Christine Lee <lee.christinedazo@gmail.com>
2019-12-26 10:10:42 +0800

    Fixed layout on report page

--- client/views/pages/main/report.js
+++ client/views/pages/main/report.js
@@ -3,11 +3,26 @@ Template.report.onCreated(function () {
 });
 
 Template.report.onRendered(function () {
-    
+    selectPropertyName();
+
+    function selectPropertyName (){
+        var properties = Properties.find({}).fetch();
+        var propertiesArr = [];
+
+        for (var i = 0; i < properties.length; i++){
+            propertiesArr.push({
+                _id : properties[i]._id,
+                propertyName: properties[i].propertyName
+            })
+            $("#propertyNames").append("<option value='"+propertiesArr[i]._id+"'>"+propertiesArr[i].propertyName+"</option>")
+        }
+    }
 });
 
 Template.report.events({
-    
+    'click #run-report': function (event, template) {
+        console.log("run")
+    }
 });
 
 Template.report.helpers({
